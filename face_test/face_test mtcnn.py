5d214a2bd22aef113f3511e36390566e32f3636a
Liza <liza_kolev@brown.edu>
2022-05-09 15:12:28 -0400

    I'm just testing random things :D

--- face_test/face_test mtcnn.py
+++ face_test/face_test mtcnn.py
@@ -5,7 +5,7 @@ from cv2 import destroyAllWindows
 import os
 
 detector = MTCNN()
-img = cv2.imread('cs143-final-facebox-anime-stylize/face_test/test2.jpg')
+img = cv2.imread('cs143-final-facebox-anime-stylize/face_test/aot.jpg')
 faces = detector.detect_faces(img)  # result
 directory = r'C:/Users/lizak\Desktop\Brown\Spring 2022/CSCI 1430/cs143-final-facebox-anime-stylize/face_test/results'
 os.chdir(directory)
@@ -15,7 +15,7 @@ for result in faces:
     x1, y1 = x + w, y + h
     cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
 
-filename = 'facebox_mtcnn.jpg'
+filename = 'facebox_mtcnn_aot.jpg'
 cv2.imwrite(filename, img)
 
 cv2.imshow('face_test_boxes', img)
--- face_test/face_test.py
+++ face_test/face_test.py
@@ -8,7 +8,7 @@ import os
 modelFile = "cs143-final-facebox-anime-stylize/face_test/res10_300x300_ssd_iter_140000.caffemodel"
 configFile = "cs143-final-facebox-anime-stylize/face_test\deploy.prototxt"
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)  # readNetFromCaffe
-im = cv2.imread('cs143-final-facebox-anime-stylize/face_test/test2.jpg')
+im = cv2.imread('cs143-final-facebox-anime-stylize/face_test/aot.jpg')
 directory = r'C:/Users/lizak\Desktop\Brown\Spring 2022/CSCI 1430/cs143-final-facebox-anime-stylize/face_test/results'
 os.chdir(directory)
 h, w = im.shape[:2]
@@ -27,7 +27,7 @@ for i in range(faces.shape[2]):
         (x, y, x1, y1) = box.astype("int")
         im = cv2.rectangle(im, (x, y), (x1, y1), (0, 0, 255), 2)
 
-filename = 'facebox_dnn.jpg'
+filename = 'facebox_dnn_aot.jpg'
 cv2.imwrite(filename, im)
 
 cv2.imshow('face_test_boxes', im)
